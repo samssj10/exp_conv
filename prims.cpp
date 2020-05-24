0e32992f9c063a9a7d562f4d3d51dc31164c4f42
samssj10 <46166721+samssj10@users.noreply.github.com>
2020-05-24 13:54:05 +0530

    fixed issue regarding size of graph

--- prims.cpp
+++ prims.cpp
@@ -4,44 +4,31 @@ using namespace std;
 
 class graph
 {
-    int n; //no of vertices
-    int e; //no of edges
-    int adj[max][max];
+    int n;//no of vertices
+    int cost[max][max];
     public:
         graph()
         {
             cout<<"enter no of vertices"<<endl;
             cin>>n;
-            cout<<"enter no of edges"<<endl;
-            cin>>e;
         }
-
         void adj_mat()
         {
-            cout<<"enter weights for :"<<endl;
             int i,j;
             for(i=0;i<n;i++)
             {
                 for(j=0;j<n;j++)
                 {
-                   adj[i][j]=0; //initial value
+                    cout<<"enter weight of edge from "<<i<<" to "<<j<<endl; //self loop = 0 , no edge = 100
+                    cin>>cost[i][j];
                 }
-            }
-
-            //entering all wts of given edges
-            for(i=0;i<e;i++)
-            {
-                int s,l,wt;
-                cout<<"enter smaller vertex , larger vertex , weight"<<endl;
-                cin>>s>>l>>wt;
-                adj[s][l]=wt;
             }
         }
 
-        int find_min(int wt[max],bool visit[max],int n)
+        int min_vertex(int wt[],bool visit[])
         {
-            int min_vertex=-1;
             int i;
+            int min_vertex=-1;
             for(i=0;i<n;i++)
             {
                 if(!visit[i] && (min_vertex==-1 || wt[i]<wt[min_vertex]))
@@ -54,48 +41,58 @@ class graph
 
         void prims()
         {
-            int par[n],wt[n],start,i,j;
+            int start,curr,near[n],wt[n],i,j;
             bool visit[n];
-            for(i=0;i<n;i++)
-            {
-                wt[i]=max;
-                visit[i]=0;
-            }
             cout<<"enter starting vertex"<<endl;
             cin>>start;
-            wt[start]=0;
-            par[start]=-1;
             for(i=0;i<n;i++)
             {
-                int min_vertex=find_min(wt,visit,n);
-                visit[min_vertex]=1;
-                for(j=0;j<n;j++)
+                if(i==start)
                 {
-                    if(adj[min_vertex][j]!=0 && !visit[j])
-                    {
-                        if(adj[min_vertex][j]<wt[j])
-                        {
-                            wt[j]=adj[min_vertex][j];
-                            par[j]=min_vertex;
-                        }
-                    }
-                }
-            }
-            for(i=1;i<n;i++)
-            {
-                if(par[i]<i)
-                {
-                    cout<<par[i]<<" "<<i<<" "<<wt[i]<<endl;
+                    near[start]=-1;
+                    visit[i]=1;
+                    wt[i]=0;
                 }
                 else
                 {
-                    cout<<i<<" "<<par[i]<<" "<<wt[i]<<endl;
+                    near[i]=start;
+                    visit[i]=0;
+                    wt[i]=max;
                 }
-                
+            }
+            curr = start;
+            for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(!visit[j])
+                    {
+                        if(cost[curr][j]<wt[j])
+                        {
+                            wt[j]=cost[curr][j];
+                            near[j]=curr;
+                        }
+                    }
+                }
+                curr=min_vertex(wt,visit);
+                visit[curr]=1;
+                if(curr==-1)
+                {
+                    break;
+                }
+                else
+                {
+                    if(curr<near[curr])
+                    {
+                        cout<<curr<<" "<<near[curr]<<" "<<wt[curr]<<endl;
+                    }
+                    else
+                    {
+                        cout<<near[curr]<<" "<<curr<<" "<<wt[curr]<<endl;
+                    }
+                }
             }
         }
-            
-
 };
 
 int main()
